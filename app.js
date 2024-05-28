a5f9f24e09c5b584a59df8851d5951532f42516d
shashankbharadwaj27 <159620388+shashankbharadwaj27@users.noreply.github.com>
2024-05-28 22:38:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const populateUser=require('./middlewares/populateUser');
 const app=express();
 const PORT=process.env.PORT || 8000;
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL)
 .then(console.log('MongoDB Connected')).catch(err=>console.log(err))
 
 app.set('view engine','ejs');
